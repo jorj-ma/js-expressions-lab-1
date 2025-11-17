3c355a12d10c38b1d60d62a40a77dd2b48324173
George Anzigale <anziegeorge4@gmail.com>
2025-11-17 09:53:48 +0300

    average temperature for the thirty days

--- index.js
+++ index.js
@@ -14,12 +14,12 @@
 //! After creating the four variables mentioned above, uncomment the following lines
 //* This way you can export them to the test file, this is essential for the tests to work
 
-module.exports = {
+// module.exports = {
     // tot_temperature_in_fahrenheit,
     // tot_temperature_in_celsius,
     // avg_temperature_in_fahrenheit,
     // avg_temperature_in_celsius
-};
+// };
 
 
 const day1TempF = 32
@@ -60,12 +60,11 @@ const celsiusToFahrenheit = (celsius) => (celsius * 9/5) + 32;
 const fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * 5/9;
 
 // Calculate total temperatures in Fahrenheit and Celsius
-const tot_temperature_in_fahrenheit = day1TempF + day3TempF + day5TempF + day7TempF + day9TempF + day11TempF + day13TempF + day15TempF + day17TempF + day19TempF + day21TempF + day23TempF + day25TempF + day27TempF + day29TempF;
-const tot_temperature_in_celsius = celsiusToFahrenheit(day2TempC) + celsiusToFahrenheit(day4TempC) + celsiusToFahrenheit(day6TempC) + celsiusToFahrenheit(day8TempC) + celsiusToFahrenheit(day10TempC) + celsiusToFahrenheit(day12TempC) + celsiusToFahrenheit(day14TempC) + celsiusToFahrenheit(day16TempC) + celsiusToFahrenheit(day18TempC) + celsiusToFahrenheit(day20TempC) + celsiusToFahrenheit(day22TempC) + celsiusToFahrenheit(day24TempC) + celsiusToFahrenheit(day26TempC) + celsiusToFahrenheit(day28TempC) + celsiusToFahrenheit(day30TempC);
-
+const tot_temperature_in_fahrenheit = (day1TempF + day3TempF + day5TempF + day7TempF + day9TempF + day11TempF + day13TempF + day15TempF + day17TempF + day19TempF + day21TempF + day23TempF + day25TempF + day27TempF + day29TempF) + (celsiusToFahrenheit(day2TempC) + celsiusToFahrenheit(day4TempC) + celsiusToFahrenheit(day6TempC) + celsiusToFahrenheit(day8TempC) + celsiusToFahrenheit(day10TempC) + celsiusToFahrenheit(day12TempC) + celsiusToFahrenheit(day14TempC) + celsiusToFahrenheit(day16TempC) + celsiusToFahrenheit(day18TempC) + celsiusToFahrenheit(day20TempC) + celsiusToFahrenheit(day22TempC) + celsiusToFahrenheit(day24TempC) + celsiusToFahrenheit(day26TempC) + celsiusToFahrenheit(day28TempC) + celsiusToFahrenheit(day30TempC));
+const tot_temperature_in_celsius = (fahrenheitToCelsius(day1TempF) + day2TempC + fahrenheitToCelsius(day3TempF) + day4TempC + fahrenheitToCelsius(day5TempF) + day6TempC + fahrenheitToCelsius(day7TempF) + day8TempC + fahrenheitToCelsius(day9TempF) + day10TempC + fahrenheitToCelsius(day11TempF) + day12TempC + fahrenheitToCelsius(day13TempF) + day14TempC + fahrenheitToCelsius(day15TempF) + day16TempC + fahrenheitToCelsius(day17TempF) + day18TempC + fahrenheitToCelsius(day19TempF) + day20TempC + fahrenheitToCelsius(day21TempF) + day22TempC + fahrenheitToCelsius(day23TempF) + day24TempC + fahrenheitToCelsius(day25TempF) + day26TempC + fahrenheitToCelsius(day27TempF) + day28TempC + fahrenheitToCelsius(day29TempF) + day30TempC) 
 // Calculate average temperatures in Fahrenheit and Celsius
-const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / 15;
-const avg_temperature_in_celsius = tot_temperature_in_celsius / 15;
+const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / 30;
+const avg_temperature_in_celsius = tot_temperature_in_celsius / 30
 
 // Console.log the results for inspection
 console.log(`Total Temperature in Fahrenheit: ${tot_temperature_in_fahrenheit}`);
